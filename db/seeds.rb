eae1c17d141851f38295473e9df2d0274c387922
AsafDavidov <adavidov@conncoll.edu>
2018-10-15 19:25:44 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Song.create({title: "hello", released: true, release_year: 1995, artist_name: "adele", genre: "RnB"})
+Song.create({title: "What", released: true, release_year: 1994, artist_name: "GAGA", genre: "hipity"})
+Song.create({title: "Where", released: true, release_year: 1996, artist_name: "nely", genre: "hopity"})
